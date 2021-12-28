110a5ef824b56864b02a36b31e9e597713a2aab3
KojimaNaoyuki <you@example.com>
2021-12-28 22:44:59 +0900

    webiopiマクロ定義完了

---
+++ main.js
@@ -0,0 +1,14 @@
+webiopi()
+
+function advance() {
+  webiopi().callMacro('advance');
+}
+function back() {
+  webiopi().callMacro('back');
+}
+function right() {
+  webiopi().callMacro('right');
+}
+function left() {
+  webiopi().callMacro('left');
+}
---
+++ script/webiopiTest.py
@@ -0,0 +1,21 @@
+import webiopi
+import subprocess
+
+@webiopi.macro
+def advance():
+	#前進
+	
+
+@webiopi.macro
+def back():
+	#後退
+	
+
+@webiopi.macro
+def right():
+	#右
+	
+
+@webiopi.macro
+def left():
+	#左
